d4c9fec41d8f0f427251ab6c8c7d5083682dea16
joel-gbsn <joel_gibson_93@hotmail.com>
2025-08-14 14:12:55 +1000

    Documentation updated for all methods in Reader class

--- QueensSolver/src/datamanagement/Reader.java
+++ QueensSolver/src/datamanagement/Reader.java
@@ -12,13 +12,22 @@ import java.util.Set;
 import ui.UserInterface;
 import util.Puzzle;
 
+/**
+ * Reader class for loading puzzles from file.
+ * @author Joel Gibson
+ */
 public class Reader {
 	
+	/**
+	 * Creates a puzzle from the given file.
+	 * @param filename the file to read
+	 * @return the puzzle
+	 */
 	public Puzzle readPuzzle(String filename) {
-		
+		// list of region labels in each row
 		List<char[]> rows = new ArrayList<>();
-		BufferedReader br = null;
 		
+		BufferedReader br = null;
 		try {	
 			br = new BufferedReader(new FileReader(new File(filename)));
 			
@@ -27,7 +36,6 @@ public class Reader {
 			while ((line = br.readLine()) != null) {
 				rows.add(line.toCharArray());
 			}
-			
 		} catch (IOException e) {
 			UserInterface.printError("could not read file.");
 			return null;
@@ -37,12 +45,13 @@ public class Reader {
 		try {
 			br.close();
 		} catch (IOException e) {
-			UserInterface.printError("could not read file.");
+			UserInterface.printError("could not close file.");
 			return null;
 		}
 		
 		char[][] grid = rows.toArray(new char[0][0]);
 		
+		// check if the puzzle is valid
 		if (!isValidDimension(grid)) {
 			UserInterface.printError("invalid puzzle dimensions.");
 			return null;
@@ -53,20 +62,38 @@ public class Reader {
 			return null;
 		}
 		
+		// create puzzle using the grid of region labels
 		return new Puzzle(grid);
 	}
 	
+	/**
+	 * Checks if the dimension of the given puzzle grid is valid (the number of rows and columns should
+	 * be equal, and the length of each row/column should be greater than 3).
+	 * @param grid the grid of region labels
+	 * @return true if grid dimension is valid, otherwise false
+	 */
 	protected boolean isValidDimension(char[][] grid) {
 		int numRows = grid.length;
+		
+		// check the length of each row
 		for (char[] row : grid) {
 			if (row.length != numRows) {
 				return false;
 			}
 		}
+		
+		// puzzles with side length less than 4 have no valid solution
 		return numRows > 3;
 	}
 	
+	/**
+	 * Checks if the number of regions in the given puzzle grid is valid (the number of regions should
+	 * be the same as the number of rows and columns).
+	 * @param grid the grid of region labels
+	 * @return true is number of regions is valid, otherwise false
+	 */
 	protected boolean isValidRegionNumber(char[][] grid) {
+		// find each region label
 		Set<Character> regions = new HashSet<>();
 		for (char[] row : grid) {
 			for (char elem : row) {
